fe1c49a4baad15ef04fbf0569d349d88fe23629d
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-02-20 11:18:12 +0100

    P5: Database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [{
-    'author': 'Author Name',
-    'title': 'Blog post 1',
-    'content': 'First post content',
-    'date_posted': 'August 27, 2018',
-    },
-    {
-    'author': 'Second Author Name',
-    'title': 'Blog post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 28, 2018',
-    },]
 
 def home(request):
   context = {
-    'posts': posts,
+    'posts': Post.objects.all(),
     }
   return render(request,'blog/home.html',context)
 
