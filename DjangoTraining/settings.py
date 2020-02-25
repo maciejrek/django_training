d80961235a2e7c4626dad731ab202345b92a9f04
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-02-25 19:25:01 +0100

    P12: Email and password reset

--- DjangoTraining/settings.py
+++ DjangoTraining/settings.py
@@ -123,3 +123,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4' #crispy
 LOGIN_REDIRECT_URL = 'blog-home' 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'maildomoichtestow@gmail.com'
+EMAIL_HOST_PASSWORD = 'zuggplicpzbnmhan'
